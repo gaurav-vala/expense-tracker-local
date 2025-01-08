e16e437764ce4c03dcb0df5249f81a117761c7ec
Gaurav <gauravvala492@gmail.com>
2025-01-08 22:23:50 +0530

    PWA Change

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,33 @@ import path from "path"
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['/vite.svg', 'robots.txt', 'apple-touch-icon.png'],
+    manifest: {
+      name: 'Expense Tracker',
+      short_name: 'Tracker',
+      description: 'An app to track expenses',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: '/vite.svg',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/vite.svg',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+  }),],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
