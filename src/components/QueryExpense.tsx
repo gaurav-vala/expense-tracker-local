cbd4a33f3e5c04eeccd0eb0aefb0d4a9bda87a39
Gaurav <gauravvala492@gmail.com>
2025-02-06 21:52:08 +0530

    date removed from listing

--- src/components/QueryExpense.tsx
+++ src/components/QueryExpense.tsx
@@ -56,13 +56,7 @@ export default function QueryExpense() {
                 </p>
                 <p>
                   <span className="flex items-center gap-1 text-xl font-semibold tracking-tighter">
-                    {expense.name} -{" "}
-                    {expense.date &&
-                      !isNaN(new Date(expense.date).getTime()) && (
-                        <span className="text-base italic font-light text-neutral-500">
-                          - {new Date(expense.date).toLocaleString()}
-                        </span>
-                      )}
+                    {expense.name}
                   </span>
                   <span className="block text-lg font-medium">
                     {" "}
@@ -127,11 +121,6 @@ export default function QueryExpense() {
                 <p className="text-wrap">
                   <span className="flex items-center w-full gap-1 text-2xl font-semibold tracking-tighter ">
                     {income.name}
-                    {income.date && (
-                      <span className="text-base italic font-light text-neutral-500">
-                        - {new Date(income.date).toLocaleString()}
-                      </span>
-                    )}
                   </span>
                   <span className="block text-lg font-medium">
                     ₹{formatCurrency(income.amount)}
